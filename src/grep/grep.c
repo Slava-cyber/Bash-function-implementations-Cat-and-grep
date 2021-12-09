58520dd33c83ae09050152f2fd9c173a1c2f8f14
Dougie Zoraida <dzoraida@se-h6.nsk.21-school.ru>
2021-12-09 23:54:15 +0700

    grep

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -5,68 +5,32 @@ int InitStruct_grep(Flags_grep *flag);
 int IndicateFlag_grep(Flags_grep *flags, char c);
 int ParserFlags_grep(Flags_grep *flags, int argc, char **argv, char *str, int *argv_number, char *search_str, int *search_number, int *count_flags);
 
+int Parser(Flags_grep *flags, int argc, char **argv, char **search_str, int *search_number, int *count_flags, int *error);
+
 int ProcessFile_grep(Flags_grep *flags, char *file, char **search_str, int search_number, int CountFile);
 int CheckLine(Flags_grep *flags, char *line, char **search_str, int search_number);
+int FileInit(int CountFile, char * file, char *FileName);
+char FlagsProccess(Flags_grep *flags, char *line, char **search_str, int search_number, char *FileName, int *CountFlagN, int *CountFlagC, int *CountFlagL, int *print);
 
 int main(int argc, char *argv[]) {
-    int order = 1, shift = 1, error = 0;
     char **search_str;
     search_str = (char**)malloc(1024 *sizeof(char*));
     for (int i = 0; i < 1024; i++) {
         *(search_str + i) = (char*) malloc(1024 * sizeof(char));
     }
-    int CountFile = 0;
-    int search_number = 0, count_flags = 0;
     Flags_grep flags;
     InitStruct_grep(&flags);
-    int i = 1;
-    // parser
-    while (i < argc) {
-        if (argv[i][0] == '-') {
-            if (ParserFlags_grep(&flags, argc, argv, argv[i], &i, search_str[search_number], &search_number, &count_flags)) {
-                error = 1;
-                break;
-            }
-        }
-        i++;
+    int search_number = 0, count_flags = 0, error = 0, CountFile;
+    if (argc > 1) {
+        CountFile = Parser(&flags, argc, argv, search_str, &search_number, &count_flags, &error);    
+    } else {
+        error = 1;
     }
-    if (!count_flags) {
-        strcat(search_str[search_number], argv[1]);
-        argv[1][0] = '-';
-        search_number += 1;  
-    } else if (flags.e == 0) {
-        i = 1; 
-        while( i < argc) {
-            if (argv[i][0] != '-') {
-                strcat(search_str[search_number], argv[i]);
-                argv[i][0] = '-';
-                search_number += 1;
-                break;  
-            } 
-            i++;    
-        }
-        if (search_number == 0)
-            error = 1; 
-    }
-    // count files
-    for (int i = 1; i < argc; i++) {
-        if (argv[i][0] != '-')
-            CountFile += 1;
-    }
-
-    
-    // printf("l:%d\n", flags.l);
-    // printf("c:%d\n", flags.c);
-
-    // printf("\ncount_flags:%d\n", count_flags);
-    // printf("str1:%s\n", search_str[0]);
-    // printf("str2:%s\n", search_str[1]);
-    // printf("str3:%s\n", search_str[2]);
     
     if (!error) {
         for (int i = 1; i < argc; i++) {
             if (argv[i][0] != '-')
-                order = ProcessFile_grep(&flags, argv[i], search_str, search_number, CountFile);
+                ProcessFile_grep(&flags, argv[i], search_str, search_number, CountFile);
         }
     }
 
@@ -76,6 +40,43 @@ int main(int argc, char *argv[]) {
     free(search_str);
     return 0;
 }
+
+int Parser(Flags_grep *flags, int argc, char **argv, char **search_str, int *search_number, int *count_flags, int *error) {
+    int CountFile = 0, i = 1;
+    while (i < argc) {
+            if (argv[i][0] == '-') {
+                if (ParserFlags_grep(flags, argc, argv, argv[i], &i, search_str[*search_number], search_number, count_flags)) {
+                    *error = 1;
+                    break;
+                }
+            }
+            i++;
+        }
+        if (!*count_flags) {
+            sprintf(search_str[*search_number], "%s", argv[1]);
+            argv[1][0] = '-';
+            *search_number += 1;  
+        } else if (flags->e == 0) {
+            i = 1; 
+            while(i < argc) {
+                if (argv[i][0] != '-') {
+                    sprintf(search_str[*search_number], "%s", argv[i]);
+                    argv[i][0] = '-';
+                    *search_number += 1;
+                    break;  
+                } 
+                i++;    
+            }
+            if (*search_number == 0)
+                *error = 1; 
+        }
+        // count files
+        for (int i = 1; i < argc; i++) {
+            if (argv[i][0] != '-')
+                CountFile += 1;
+        }
+    return CountFile;
+} 
 
 int InitStruct_grep(Flags_grep *flag) {
     flag->e = 0;
@@ -90,23 +91,20 @@ int InitStruct_grep(Flags_grep *flag) {
 int ParserFlags_grep(Flags_grep *flags, int argc, char **argv, char *str, int *argv_number, char *search_str, int *search_number, int *count_flags) {
     int error = 0, i = 1; 
         while (str[i] != '\0') {
-            // printf("%c", str[i]);
             if (IndicateFlag_grep(flags, str[i])) {
                 error = 1;
                 break;
             } else {
-                // printf("in:%c", str[i]);
                 *count_flags += 1;
                 if (str[i] == 'e') {
                     if (str[i + 1] != '\0') {
-                        // printf("tek:%c\n", str[i]);
                         memmove(search_str, str + i + 1, strlen(str) - i);
                     } else if (*argv_number + 1 < argc) {
-                        strcat(search_str, argv[*argv_number + 1]);
+                        sprintf(search_str, "%s", argv[*argv_number + 1]);
                         argv[*argv_number + 1][0] = '-';
                         *argv_number += 1;
                     } else {
-                        printf("grep: options requires an argument :e");
+                        printf("grep: options requires an argument: e\n");
                         error = 1;
                         break;
                     }
@@ -116,7 +114,6 @@ int ParserFlags_grep(Flags_grep *flags, int argc, char **argv, char *str, int *a
             }
             i++;
         }
-        // printf("\n");
     return error;
 }
 
@@ -149,6 +146,27 @@ int IndicateFlag_grep(Flags_grep *flags, char c) {
     return error;
 }
 
+char FlagsProccess(Flags_grep *flags, char *line, char **search_str, int search_number, char *FileName, int *CountFlagN, int *CountFlagC, int *CountFlagL, int *print) {
+    char last = 'q';
+    *print = 0;
+    *CountFlagN += 1;
+    if (CheckLine(flags, line, search_str, search_number)) {
+        if (flags->l)
+            *CountFlagL += 1;
+        if (flags->c)
+            *CountFlagC += 1;
+        if (!flags->c && !flags->l) {
+            if (flags->n)
+                printf("%s%d:%s", FileName, *CountFlagN, line);
+            else
+                printf("%s%s", FileName, line);
+            }
+        *print += 1;
+    }
+    last = line[strlen(line) - 1];
+    return last;
+}
+
 
 int ProcessFile_grep(Flags_grep *flags, char *file, char **search_str, int search_number, int CountFile) {
     char *line = NULL;
@@ -158,35 +176,16 @@ int ProcessFile_grep(Flags_grep *flags, char *file, char **search_str, int searc
     char *FileName = (char*) malloc(512 * sizeof(char));
     FILE *fp;
     fp = fopen(file, "r");
+    int print = 0;
     if (fp == NULL) {
-        printf("grep: %s: No such file or directory", file);
+            printf("grep: %s: No such file or directory\n", file);
     } else {
-        if (CountFile > 1) {
-            strcat(FileName, file);
-            FileName[strlen(file)] = ':';
-            FileName[strlen(file) + 1] = '\0';
-        } else {
-            FileName[0] = '\0';
-        }
+        FileInit(CountFile, file, FileName);
         int CountFlagC = 0, CountFlagN = 0, CountFlagL = 0;
+        char last = 'q';
         while (getline(&line, &len, fp) != -1) {
-            CountFlagN += 1;
-            if (CheckLine(flags, line, search_str, search_number)) {
-                if (flags->l)
-                    CountFlagL += 1;
-                if (flags->c)
-                    CountFlagC += 1;
-                if (!flags->c && !flags->l) {
-                    if (flags->n)
-                        printf("%s%d:%s", FileName, CountFlagN, line);
-                    else
-                        printf("%s%s", FileName, line);
-                }
-            }
+            last = FlagsProccess(flags, line, search_str, search_number, FileName, &CountFlagN, &CountFlagC, &CountFlagL, &print);
         }
-        // printf("l:%d\n", CountFlagL);
-        // printf("c:%d\n", flags->c);
-        // printf("countc:%d\n", CountFlagC);
 
         if (flags->c) {
             if (!CountFlagL) {
@@ -199,11 +198,24 @@ int ProcessFile_grep(Flags_grep *flags, char *file, char **search_str, int searc
         } else if (CountFlagL) { 
                 printf("%s\n", file);
         }
+        if (last != '\n' && print != 0 && flags->c == 0  && flags->l == 0)
+            printf("\n");
+        fclose(fp);
     }
     free(FileName);
-    fclose(fp);
     return 0;
-}       
+}
+
+int FileInit(int CountFile, char * file, char *FileName) {
+        if (CountFile > 1) {
+            sprintf(FileName, "%s", file);
+            FileName[strlen(file)] = ':';
+            FileName[strlen(file) + 1] = '\0';
+        } else {
+            FileName[0] = '\0';
+        }
+    return 1;
+}
 
 int CheckLine(Flags_grep *flags, char *line, char **search_str, int search_number) {
     int success = 0;
